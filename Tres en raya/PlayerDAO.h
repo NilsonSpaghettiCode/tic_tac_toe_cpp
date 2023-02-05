ac3151414ef23c6914d34991b390226db74f8b7e
unknown <nilsonvargas136@gmail.com>
2023-02-05 17:08:26 -0500

    format code

--- Tres en raya/PlayerDAO.h
+++ Tres en raya/PlayerDAO.h
@@ -6,22 +6,22 @@ class PlayerDAO
 private:
 
 public:
-	virtual std::list<Player>getAllPlayers() = 0;
-	virtual Player* getPlayer(long id) = 0;
-	virtual void createPlayer(Player *& player) = 0;
-	virtual void updatePlayer(Player*& player) = 0;
+	virtual std::list<Player> getAllPlayers()			     = 0;
+	virtual Player*			  getPlayer(long id)			 = 0;
+	virtual void			  createPlayer(Player *& player) = 0;
+	virtual void			  updatePlayer(Player *& player) = 0;
 };
 
-class ImplPlayerDAO
+class ImplPlayerDAO : public PlayerDAO
 {
 public:
 	ImplPlayerDAO();
 	~ImplPlayerDAO();
 
-	std::list<Player>getAllPlayers();
-	Player* getPlayer(long id);
-	void createPlayer(Player*& player);
-	void updatePlayer(Player*& player);
+	std::list<Player> getAllPlayers()               override;
+	Player*           getPlayer(long id)			override;
+	void              createPlayer(Player*& player) override;
+	void              updatePlayer(Player*& player) override;
 
 private:
 
